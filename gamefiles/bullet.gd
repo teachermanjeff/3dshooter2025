97b231d7d50693f5bc13ba4b56ddba05704ba73e
BenMyhillJones <161653222+BenMyhillJones@users.noreply.github.com>
2025-04-03 22:10:54 -0700

    Created Gun and Sounds
    Added gun animations, bullets and sounds to the gun. Works well

---
+++ gamefiles/bullet.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+@export var speed: float = 100.0  
+
+func _ready():
+	linear_velocity = -global_transform.basis.z * speed  
+	set_as_top_level(true)  # Detach bullet from parent to prevent unwanted movement
+
+func _on_body_entered(body):
+	queue_free()  # Destroy bullet on impact
