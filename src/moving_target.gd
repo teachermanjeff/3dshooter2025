64b709721be3d46bf461c6c9af6329bd5c0c5ddf
rocketcamel-2 <luca_lise+rocketcamel@icloud.com>
2025-05-01 14:52:36 -0700

    feat: moving target

--- src/moving_target.gd
+++ src/moving_target.gd
@@ -2,16 +2,15 @@ extends Node3D
 
 var speed = 5.0
 var direction = Vector3.BACK
+var origin = transform.basis
 var distance = 100
-var end_distance = global_position
+@onready var target: Area3D = $/"Sketchfab_model/Collada visual scene group/pCube3/Area3D"
+var nodes = 0
 
 func _physics_process(delta):
+	nodes += 1
 	translate(direction * speed * delta)
-
-func _on_LeftLimit_body_entered(body):
-	if body == self:
-		direction = Vector3.BACK
-
-func _on_RightLimit_body_entered(body):
-	if body == self:
-		direction = Vector3.FORWARD
+	
+	if nodes > 60:
+		nodes = 0
+		direction = Vector3.FORWARD if direction == Vector3.BACK else Vector3.BACK
