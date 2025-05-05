d71201110cb0e9227b2e50ca2b2ec04b76e925fd
rocketcamel-2 <luca_lise+rocketcamel@icloud.com>
2025-05-05 13:53:17 -0700

    feat: target hit effect

--- src/target.gd
+++ src/target.gd
@@ -1,9 +1,23 @@
-extends Node
+extends Area3D
+
+@onready var root_parent = self.get_parent().get_parent().get_parent().get_parent()
 
 func _on_body_entered(body: Node3D):
-	await get_tree().create_timer(randf_range(0.1, 0.5)).timeout
-	if body.name == "Bullet":
-		State.set_targets_hit(State.targets_hit + 1)
-		body.queue_free()
-		await get_tree().create_timer(0.3).timeout
-		$ding.play()
+	if body.name != "Bullet":
+		return
+	var tween = get_tree().create_tween()
+	var reverse = get_tree().create_tween()
+	tween.tween_property(root_parent, "rotation_degrees", Vector3(0, 0, 90), 0.25)
+	tween.set_ease(tween.EASE_OUT_IN)
+	tween.set_trans(tween.TRANS_ELASTIC)
+	reverse.tween_property(root_parent, "rotation_degrees", Vector3(0, 0, 0), 0.25)
+	reverse.stop()
+	tween.tween_callback(func f():
+		print("[Tween] reversing")
+		await get_tree().create_timer(1).timeout
+		reverse.play()
+	)
+	State.set_targets_hit(State.targets_hit + 1)
+	body.queue_free()
+	await get_tree().create_timer(0.3).timeout
+	$ding.play()
