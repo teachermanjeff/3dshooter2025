ed6e04ae61c65b7fd484cb211f381ea3e55a928e
rocketcamel-2 <luca_lise+rocketcamel@icloud.com>
2025-05-05 13:24:54 -0700

    fix: delay target starts

--- src/target.gd
+++ src/target.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _on_body_entered(body: Node3D):
+	await get_tree().create_timer(randf_range(0.1, 0.5)).timeout
 	if body.name == "Bullet":
 		State.set_targets_hit(State.targets_hit + 1)
 		body.queue_free()
