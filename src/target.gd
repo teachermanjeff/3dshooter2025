fc974ac0b6521517afed2f62b5d2432f1ef91f80
rocketcamel-2 <166667827+rocketcamel-2@users.noreply.github.com>
2025-05-06 14:29:11 -0700

    feat!: added easing style to target, removed joystick (#21)

--- src/target.gd
+++ src/target.gd
@@ -7,9 +7,9 @@ func _on_body_entered(body: Node3D):
 		return
 	var tween = get_tree().create_tween()
 	var reverse = get_tree().create_tween()
-	tween.tween_property(root_parent, "rotation_degrees", Vector3(0, 0, 90), 0.25)
-	tween.set_ease(tween.EASE_OUT_IN)
-	tween.set_trans(tween.TRANS_ELASTIC)
+	var prop = tween.tween_property(root_parent, "rotation_degrees", Vector3(0, 0, 90), 0.8)
+	prop.set_ease(Tween.EASE_OUT)
+	prop.set_trans(Tween.TRANS_BACK)
 	reverse.tween_property(root_parent, "rotation_degrees", Vector3(0, 0, 0), 0.25)
 	reverse.stop()
 	tween.tween_callback(func f():
