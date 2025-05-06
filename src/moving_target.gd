6823730af05fa1abf8da4ce1f75fc0bec9ad50ef
rocketcamel-2 <166667827+rocketcamel-2@users.noreply.github.com>
2025-05-06 14:04:40 -0700

    feat: target effects (#20)
    * fix: remove hit by log
    
    * feat: moving target
    
    * fix: delay target starts
    
    * feat: target hit effect

--- src/moving_target.gd
+++ src/moving_target.gd
@@ -4,10 +4,18 @@ var speed = 5.0
 var direction = Vector3.BACK
 var origin = transform.basis
 var distance = 100
+var enabled = false
 @onready var target: Area3D = $/"Sketchfab_model/Collada visual scene group/pCube3/Area3D"
 var nodes = 0
 
+func _ready():
+	await get_tree().create_timer(randf_range(0.1, 1)).timeout
+	enabled = true
+
 func _physics_process(delta):
+	if not enabled:
+		return
+
 	nodes += 1
 	translate(direction * speed * delta)
 	
